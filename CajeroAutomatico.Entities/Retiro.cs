46889f14efe8ce942301f948fefc6105e6d8d5ce
Jorge Ccochayhua <jorge_ccochayhua@usmp.pe>
2017-05-16 13:15:12 -0500

    agregando referencias

--- CajeroAutomatico.Entities/Retiro.cs
+++ CajeroAutomatico.Entities/Retiro.cs
@@ -6,7 +6,9 @@ using System.Threading.Tasks;
 
 namespace CajeroAutomatico.Entities
 {
-    public class Class1
+    public class Retiro
     {
+        public int RetiroId { get; set; }
+        public int Cantidad { get; set; }
     }
 }
